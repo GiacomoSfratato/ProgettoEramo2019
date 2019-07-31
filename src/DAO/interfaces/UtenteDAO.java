974dcd82f54b82bafa741d5b6c102019268775f5
Dawid Pado <dawidpado97@gmail.com>
2019-07-31 09:52:44 +0200

    update utente interface

--- src/DAO/interfaces/UtenteDAO.java
+++ src/DAO/interfaces/UtenteDAO.java
@@ -1,27 +1,34 @@
 package DAO.interfaces;
-import java.util.List;
-
+import java.util.*;
 import model.Utente;
 public interface UtenteDAO {
-
 	 
 	/**
 	 * L'interfaccia DAO per le diverse implementazioni di CustomerDAO. Definisce le operazioni CRUD.
 	 */
 	 
-		/** Recupera tutti gli oggetti Customer dal DB. */
+		public boolean set_modifica_tipo_utente(String email, String tipo);
+		public ArrayList<?> get_utenti_attivi();
+		public String get_mostra_nome_utente();
+		public boolean set_inserimento_utente(Utente utente);
+		public boolean set_rimovere_utente(Utente utente);
+		
+	/*
+	 * L'interfaccia DAO per le diverse implementazioni di CustomerDAO. Definisce le operazioni CRUD.
+	 
+		Recupera tutti gli oggetti Customer dal DB. 
 		public List getAllCustomers();
 		
-		/** Recupera un oggetto Customer esistente a partire dall'id. */
+		Recupera un oggetto Customer esistente a partire dall'id.
 		public Utente getCustomer(int id);
 		
-		/** Crea un oggetto Customer e restituisce l'id. */
+		Crea un oggetto Customer e restituisce l'id. 
 		public int createCustomer(Utente customer);
 		
-		/** Aggiorna un oggetto Customer esistente. */
+		Aggiorna un oggetto Customer esistente.
 		public boolean updateCustomer(Utente customer);
 		
-		/** Cancella un oggetto Customer esistente. */
-		public boolean deleteCustomery(Utente customer);
+		Cancella un oggetto Customer esistente. 
+		public boolean deleteCustomery(Utente customer); */
 	}
 
