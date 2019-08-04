ed7fddb53589101599b6bb8008ad10f5aec924ff
xbit1 <xbit1@DESKTOP-OQ4EV63>
2019-08-04 13:54:12 +0200

    Main, DI NUOVO

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,32 @@
 package application;
 
 import DAO.interfaces.UtenteDAO;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import DAO.implementations.*;
+import model.*;
 
-public class Main {
+public class Main extends Application{
+	
+	public static Stage stage;
+	
+	//metodo per avviare la GUI al momento disattivato
+	@Override
+	public void start(Stage stage) throws Exception {
+	        this.stage = stage; // initialize value of stage.
+	    Parent root = FXMLLoader.load(getClass().getResource("/view/HomePage.fxml"));
+	    Scene scene = new Scene(root);
+	    stage.setTitle("Benvenuto nella Biblioteca!");
+	    
+	    stage.setScene(scene);
+	    stage.show();
+	}
+	
 	public static void main(String[] argv) {
-	UtenteDAO utente = new Utente.Builder().withmail("ciao@cia.ex").withpassword("marks124").build(); // example of utente
+	Utente utente = new Utente.Builder().withmail("ciao@cia.ex").withpassword("marks124").build(); // example of utente
 	System.out.println(utente.toString()); 
 	MySQLUtenteDAOImpl x = new MySQLUtenteDAOImpl();
 	System.out.println(x.get_utenti_attivi()); 
