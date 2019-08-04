8c3d82bf6d20cc7296b381cf7ea3b12078d26cd0
xbit1 <xbit1@DESKTOP-OQ4EV63>
2019-08-04 13:19:37 +0200

    Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,8 @@
 
 package application;
-	
+
+import model.*;
+import DAO.implementations.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -26,6 +28,27 @@ public class Main extends Application {
 	    stage.setScene(scene);
 	    stage.show();
 	}
+	
+	public static void main(String[] args) {
+		//launch(args);
+		Utente utente = new Utente.Builder().withmail("ciao@cia.ex").withpassword("marks124").build(); // example of utente
+		System.out.println(utente.toString()); 
+		MySQLUtenteDAOImpl x = new MySQLUtenteDAOImpl();
+		System.out.println(x.get_utenti_attivi()); 
+	//	System.out.println(x.set_inserimento_utente(utente)); 
+	//	System.out.println(x.set_rimovere_utente(utente));
+		System.out.println(x.get_mostra_nome_utente(6));  
+		
+		MySQLPubblicazioneDAOImpl y = new MySQLPubblicazioneDAOImpl();
+		/*System.out.print(y.get_ultime_publicazioni());
+		System.out.print(y.get_update_recente()); 
+		
+		System.out.println(y.get_pubblicazione_utente(new Utente.Builder().withmail("fulviolapenna@gmail.com").build()));*/
+	}
+	
+	//public static void main(String[] args) {
+	//	launch(args);
+	//}
 }
 
 
@@ -58,9 +81,7 @@ public class Main extends Application {
 		}
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	
 	
 	/*@Override
 	public void start(Stage primaryStage) {
