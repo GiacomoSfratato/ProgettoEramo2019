ef49e84a5ba6f992806e057e650b715ccb324369
Dawid Pado <dawidpado97@gmail.com>
2019-08-06 11:15:22 +0200

    aggiunta ultime query

--- src/DAO/implementations/MySQLPubblicazioneDAOImpl.java
+++ src/DAO/implementations/MySQLPubblicazioneDAOImpl.java
@@ -13,8 +13,11 @@ public class MySQLPubblicazioneDAOImpl implements PubblicazioneDAO {
 	private static String catalogo = "CALL catalogo";
 	private static String estrazione_dati = "CALL estrazione_dati(?)";
 	private static String cerca = "CALL cerca_pubblicazione(?,?,?,?,?)";
-	
-	
+	private static String catalogo_ristampa="CALL catalogo_ristampa";
+	private static String inserimento_like="CALL inserimento_like"; // forse va nel utenteDAOImpl
+	private static String estrazione_modifiche_pubblicazione = "CALL estrazione_modifiche_pubblicazione(?)";
+	private static String likes_totali="CALL likes_totali";
+	private static String elenco_download="CALL elenco_download";
 	@Override
 	public ArrayList<Pubblicazione> get_ultime_pubblicazioni(){
 		ArrayList <Pubblicazione> pubblicazioni = new ArrayList<Pubblicazione>();
@@ -182,7 +185,7 @@ return pubblicazioni;}
 	        }  
 		}
 		catch(Exception exc) {
-		System.out.print("Qualcosa è andato storto!"); }
+		System.out.print("Qualcosa ï¿½ andato storto!"); }
 		finally { 
 		try {
 	         ps.close();
@@ -245,43 +248,212 @@ return pubblicazioni;}
 	
 	@Override
 	public ArrayList<Pubblicazione> get_catalogo_ristampa() {
-		// TODO Auto-generated method stub
-		return null;
+	ArrayList<Pubblicazione> pubblicazioni = new ArrayList<Pubblicazione>();
+		
+		PreparedStatement ps = null;
+		ResultSet result = null;
+		Connection conn = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(catalogo_ristampa);
+			ps.execute();
+			result = ps.getResultSet();
+			  while (result.next()) { 
+				  pubblicazioni.add(new Pubblicazione.Builder().withid(result.getInt(1)).withtitolo(result.getString(2)).withdescrizione(result.getString(3)).witheditore(result.getString(4)).withdata(result.getString(5)).build());
+	        }  
+		}
+		catch(Exception exc) {
+		System.out.print("somethink goes wrong!"); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return pubblicazioni;
 	}
-
+	
 	
 	@Override
 	public ArrayList<Pubblicazione> get_catalogo_stessi_autori(Pubblicazione pubblicazione) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Pubblicazione> pubblicazioni = new ArrayList<Pubblicazione>();
+		PreparedStatement ps = null;
+		ResultSet result = null;
+		Connection conn = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(cerca);
+			ps.setInt(1,pubblicazione.getId());
+			ps.execute();
+			result = ps.getResultSet();
+			  while (result.next()) {     
+			   		pubblicazioni.add(new Pubblicazione.Builder()
+			   				.withid(result.getInt(1))
+			   				.withtitolo(result.getString(2))
+			   				.withdescrizione(result.getString(3))
+			   				.witheditore(result.getString(4))
+			   				.build());
+	        }  
+		}
+		catch(SQLException exc) {
+		System.out.print(exc.getMessage()); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return pubblicazioni;
 	}
+	
 
 	
 	@Override
 	public boolean set_inserimento_like(String email, String password, int id_pubblicazione) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean fatto = false;
+		PreparedStatement ps = null;
+		Connection conn = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(inserimento_like);
+			ps.setString(1,email);
+			ps.setString(2,password);
+			ps.setInt(3,id_pubblicazione);
+			ps.execute();
+			fatto = true;
+		}
+		catch(SQLException exc) {
+		System.out.print(exc.getMessage()); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return fatto;
 	}
-
+	
 	
 	@Override
 	public Pubblicazione get_likes_totali(Pubblicazione pubblicazione) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet result = null;
+		Connection conn = null;
+		Pubblicazione pubblicazioni = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(likes_totali);
+			ps.setInt(1,pubblicazione.getId());
+			ps.execute();
+			result = ps.getResultSet();
+			pubblicazioni = new Pubblicazione.Builder()
+			   				.withid(result.getInt(1))
+			   				.withtitolo(result.getString(2))
+			   				.withlikes_totali(result.getInt(3))
+			   				.build();    
+		}
+		catch(SQLException exc) {
+		System.out.print(exc.getMessage()); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return pubblicazioni;
+		
 	}
-
+	
 	
 	@Override
 	public ArrayList<Storico> get_estrazione_modifiche_pubblicazione(Pubblicazione pubblicazione) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Storico> storico = new ArrayList<Storico>();
+		PreparedStatement ps = null;
+		ResultSet result = null;
+		Connection conn = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(estrazione_modifiche_pubblicazione);
+			ps.setInt(1,pubblicazione.getId());
+			ps.execute();
+			result = ps.getResultSet();
+			  while (result.next()) {            	
+				  storico.add(new Storico(result.getString(3),result.getString(4),result.getInt(1),result.getInt(2)));
+	        }  
+		}
+		catch(Exception exc) {
+		System.out.print("Qualcosa ï¿½ andato storto!"); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return storico;
 	}
 
 	
 	@Override
 	public ArrayList<Pubblicazione> get_elenco_download() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Pubblicazione> pubblicazioni = new ArrayList<Pubblicazione>();
+		PreparedStatement ps = null;
+		ResultSet result = null;
+		Connection conn = null;
+		try {
+			conn = MySQLDAOFactory.createConnection();
+			ps = conn.prepareStatement(elenco_download);
+			ps.execute();
+			result = ps.getResultSet();
+			  while (result.next()) {            	
+				  ArrayList<Sorgente> sorg = new ArrayList<Sorgente>();
+				  sorg.add(new Sorgente(result.getString(4)));
+			   		pubblicazioni.add(new Pubblicazione.Builder()
+			   				.withtitolo(result.getString(1))
+			   				.withdescrizione(result.getString(2))
+			   				.witheditore(result.getString(3))
+			   				.withsorgenti(sorg)
+			   				.build());
+	        }  
+		}
+		catch(Exception exc) {
+		System.out.print("Qualcosa ï¿½ andato storto!"); }
+		finally { 
+		try {
+	         ps.close();
+	     } catch (Exception sse) {
+	      	System.out.println("preparedStatement.close();"); 
+	     }
+	     try {
+	         conn.close();
+	     } catch (Exception cse) {
+	     	System.out.println("conn.close();");
+	     }  }
+		return pubblicazioni;
 	}
 
 }
