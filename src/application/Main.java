f596bf95b95c0fd59e41f00f27334cb501cd58e0
Dawid Pado <dawidpado97@gmail.com>
2019-08-09 13:26:49 +0200

    convert to maven, adding amazon s3

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import DAO.MySQLDAOFactory;
 import DAO.implementations.*;
@@ -49,7 +58,38 @@ public class Main extends Application{
 	System.out.println(y.get_pubblicazione_utente(new Utente.Builder().withmail("fulviolapenna@gmail.com").build()));
 	System.out.println(y.get_catalogo());
 	*/
-	
-	
+ /*   Regions clientRegion = Regions.EU_WEST_1;
+    String bucketName = "bibliotecaesame";
+    String stringObjKeyName = "txt";
+    String fileObjKeyName = "foto.jpg";
+    String fileName = "/home/dawpad/Desktop/objprova.jpg";
+
+    try {
+        //This code expects that you have AWS credentials set up per:
+        // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(clientRegion)
+                .build();
+
+        // Upload a text string as a new object.
+        s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+
+        // Upload a file as a new object with ContentType and title specified.
+        PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("plain/text");
+        metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+        request.setMetadata(metadata);
+        s3Client.putObject(request);
+    } catch (AmazonServiceException e) {
+        // The call was transmitted successfully, but Amazon S3 couldn't process 
+        // it, so it returned an error response.
+        e.printStackTrace();
+    } catch (SdkClientException e) {
+        // Amazon S3 couldn't be contacted for a response, or the client
+        // couldn't parse the response from Amazon S3.
+        e.printStackTrace();
+    }
+*/	
 }
 }
